8b8150c36f0185c9e0b0f603db854065c8bd605a
SENA <SENA@MEDAPRCSGFSP539>
2023-08-23 11:36:28 -0500

    Agregar archivos de proyecto.

---
+++ Crud Entity/Data/AplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Crud_Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_Entity.Data
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+
+}
---
+++ Crud Entity/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud_Entity.Models
+{
+    public class Movie
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Gender { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
